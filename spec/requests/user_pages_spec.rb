28786eb842c21057ffa4af17abcb9f020328052b
Lucas <lucaslessa@gmail.com>
2013-10-26 00:01:34 -1000

    Add gravatar size, sign up error and success tests, neater tag for flash

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,20 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+	  describe "after submission" do
+        before { click_button submit }
+        let(:page_title) { 'Sign Up' }
+        error_list = ["Name can't be blank", "Email can't be blank",
+					  "Email is invalid", "Password can't be blank",
+					  "Password is too short"]
+
+        it { should have_title(full_title(page_title)) }
+        it { should have_content('error') }
+        error_list.each do |error_message|
+        	it { should have_content(error_message) }
+        end
+      end
     end
 
     describe "with valid information" do
@@ -43,6 +57,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+	  describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
