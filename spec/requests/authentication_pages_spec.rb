a5131b12120c236bea12936d5969c3a7981c25bb
Lucas <lucaslessa@gmail.com>
2013-10-26 17:09:35 -1000

    Decouple some tests from implementation

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,24 +19,19 @@ describe "Authentication" do
 
     describe "with invalid information" do
       before { click_button signin_button }
-      let(:error_selector) { 'div.alert.alert-error' }
 
       it { should have_title(signin_title) }
-      it { should have_selector(error_selector, text: 'Invalid') }
+      it { should have_error_message('Invalid') }
 
 	  describe "after visiting another page flashes error" do
 	  	before { click_link 'Home' }
-	  	it { should_not have_selector(error_selector) }
+	  	it { should_not have_error_message('') }
 	  end
     end
 
 	describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button signin_button
-      end
+      before { valid_signin(user) }
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
@@ -45,7 +40,7 @@ describe "Authentication" do
 
 	  describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        it { should have_link(signin_title) }
       end
     end
   end
